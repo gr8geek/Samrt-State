b6f1978efeec446cb541c3c8b688ffa4808d2aef
gr8geek <pratyush9000@gmail.com>
2021-02-22 03:40:58 +0530

    Initial Commit

---
+++ stateStore.js
@@ -0,0 +1,45 @@
+class classListCollection{
+    constructor(props){
+        let classList = []
+        let elements = []
+    }
+    updateClassList(){
+        
+    }
+    updateElementMap(){
+
+    }
+}
+class Component{
+    Componenent(props){
+        var end = ()=>{
+        
+        }
+        var remove =(fun)=>{
+            fun()
+        }
+        var type = props.type
+        var id = props.id
+        var classList = props.classList
+        var newElement = document.createElement(type)
+        newElement.id = id
+        newElement.classList.add(...classList)
+
+        return newElement
+    }
+    updator(){
+
+    }
+}
+class Container{
+    constructor(props)
+    {
+
+        let state = new Map();
+        let handler = new Map();
+        let component = []
+        let listContainer = new Map();
+
+    }
+
+}
